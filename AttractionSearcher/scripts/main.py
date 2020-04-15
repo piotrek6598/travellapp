e83d30a6bed7216da0fab8ea0c324e4beea3c375
MixaDixa <mikolajdrzewiecki.1999@gmail.com>
2020-04-15 11:58:15 +0200

    added parent directory

--- AttractionSearcher/scripts/main.py
+++ AttractionSearcher/scripts/main.py
@@ -4,4 +4,4 @@ from presearch import Presearch
 location = input("enter location")
 pages = input("enter number of desired pages")
 searcher = Presearch()
-resuts = searher.get_places_query(location, pages)
+resuts = searcher.get_places_query(location, pages)
