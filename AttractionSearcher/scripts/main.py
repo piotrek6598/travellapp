d010f9c51d16de27ddf7e575ac9a6029a72ec475
MixaDixa <mikolajdrzewiecki.1999@gmail.com>
2020-04-15 12:06:40 +0200

    main.py input parameters

--- AttractionSearcher/scripts/main.py
+++ AttractionSearcher/scripts/main.py
@@ -1,7 +1,11 @@
 from presearch import Presearch
+import sys
 
 
-location = input("enter location")
-pages = input("enter number of desired pages")
-searcher = Presearch()
-resuts = searcher.get_places_query(location, pages)
+try:
+    location = sys.argv[1]  # first param - [0] is an executed path
+    pages = sys.argv[2]
+    searcher = Presearch()
+    resuts = searcher.get_places_query(location, pages)
+except IndexError:
+    print("not enough arguments (excepted 2: location, pages)")
