b9e5195049240bccba3e02ba97345be204576cab
Madridista777 <57012166+Madridista777@users.noreply.github.com>
2020-04-15 20:52:50 +0200

    Good searcher with photos

---
+++ AttractionSearcher/main.py
@@ -0,0 +1,81 @@
+import json, sys
+import search
+
+def printPlace(place):
+    if 'name' not in place:
+        exit(1)
+    print(place["name"])
+    if 'formatted_address' not in place:
+        exit(1)
+    print('Address:', end = ' ')
+    print(place["formatted_address"])
+    if 'photos' in place and len(place["photos"]) > 0:
+        print('Photos:', end= ' ')
+        print(place["photos"][0])
+    if 'rating' not in place:
+        exit(1)
+    print('Google Maps rating:', end = ' ')
+    print(place["rating"])
+
+def goodPlace(place, tags, rating):
+    if 'types' not in place:
+        exit(1)
+    tagsOk = False
+    if len(tags) == 0:
+        tagsOk = True
+    for t in tags:
+        if t in place["types"]:
+            tagsOk = True
+    if not tagsOk:
+        return False
+    if 'rating' not in place:
+        exit(1)
+    r = float(place["rating"])
+    return r >= rating
+
+try:
+    searcher = search.Searcher()
+    location = sys.argv[1]
+    pages = 3
+    tags = []
+    rating = 0.0
+    i = 2
+    while i < len(sys.argv) - 2:
+        tags.append(sys.argv[i])
+        i += 1
+    if sys.argv[i] != 'none':
+        rating = float(sys.argv[i])
+    i += 1
+    results = int(sys.argv[i])
+    text = searcher.get_places_query(location.replace("_"," "), pages)
+    data = json.loads(text)
+    if len(tags) == 0 and rating == 0.0:
+        print('Top results:')
+        i = 0
+        places = len(data)
+        while i < places and i < results:
+            printPlace(data[i])
+            i += 1
+    else:
+        places = len(data)
+        i = 0
+        good = 0
+        while i < places and good < results:
+            if (goodPlace(data[i], tags, rating)):
+                if good == 0:
+                    print('Top results:')
+                good += 1
+                printPlace(data[i])
+            i += 1
+        if good == 0:
+            print('No results matching.')
+    
+
+except IndexError:
+    print("not enough arguments (excepted at least 3: [location], [any number of tags] [rating] [results])")
+except ValueError:
+    print("invalid literal for [rating] or [results] parameter")
+except KeyError:
+    print("No results matching.")
+
+    
