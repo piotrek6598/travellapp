3c94dfaa5b131f4ce44fdaca053e97b80111f47a
Madridista777 <57012166+Madridista777@users.noreply.github.com>
2020-04-16 22:57:01 +0200

    Update distance.py

--- bestRoute/distance.py
+++ bestRoute/distance.py
@@ -12,7 +12,7 @@ def toMinutes(secs):
 # Calculates how much time (in minutes) we need to get from source to destination,
 # using Google Distance Matrix API. Returns -1 if any of locations is invalid.
 def distance(source, destination):
-    gmaps = googlemaps.Client(key='AIzaSyCiCUMr22NKSzfi8nOVR3UFcym-j22oOcY')
+    gmaps = googlemaps.Client(key='AIzaSyBDZVlY0bsQ_5EawyjPX7oJOUDELSyPZUY')
     x = gmaps.distance_matrix(source, destination)
     if 'rows' not in x:
         return -1
