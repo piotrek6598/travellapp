2fe2d3d8061e2b0fe9127d4d283a4c4017918c34
lucciobianco <44978955+lucciobianco@users.noreply.github.com>
2020-05-13 22:34:35 +0200

    Unnecesarry comments deleted

--- accomodation.py
+++ accomodation.py
@@ -3,11 +3,10 @@ from selenium.webdriver.chrome.options import Options
 from time import sleep
 import sys
 import math
+
 #sortBy polecane - 1, cena - 2, ocena - 4
 #dates in format yyyy-mm-dd
 #ignores noChilds
-#resultSize 1 - 4 na razie bo zdjęcia się nie ładują xd (poprawię)
-
 
 def get_hotels(place, date1, date2, noAdults, noChilds, resultSize, maxPrice, sortBy):
 
@@ -46,32 +45,27 @@ def get_hotels(place, date1, date2, noAdults, noChilds, resultSize, maxPrice, so
         roomType = 9
 
     index = url.find('iRoomType=')
-    #print(index)
     url = url[:(index + 10)] + str(roomType) + url[(index + 11):]
     #print(url)
 
 
     index = url.find('adults%5D=')
-    #print(index)
     url = url[:(index + 10)] + str(noAdults) + url[(index + 11):]
-    #print(url)
 
 
     index = url.find('sortingId=')
-    #print(index)
+
     url = url[:(index + 10)] + str(sortBy) + url[(index + 11):]
-    #print(url)
+
     driver.get(url)
     sleep(8)
     hotelNames = driver.find_elements_by_xpath('//ol/li/div/article/div[1]/div[2]/div/div/h3/span')
-                                               #/html/body/div[4]/main/div[1]/div/div[4]/div/div/div[2]/div[1]/div[1]/section/ol/li[1]/article/div[1]/div[2]/div/div/h3/span
+                                              
     hotelTypes = driver.find_elements_by_xpath('//ol/li//p[@data-qa="accommodation-type"]')
-    #/html/body/div[4]/main/div[1]/div/div[4]/div/div/div[2]/div[1]/div[1]/section/ol/li[12]/div/article/div[1]/div[2]/div/div/div[1]/p
+
     hotelPhotos = driver.find_elements_by_xpath('//ol/li/div/article/div[1]/div[1]/div[2]/div/img')
     hotelPrices = driver.find_elements_by_xpath('//ol/li/div/article/div[1]/div[2]/section/div[2]/article/div/div[2]/div/div/div/strong')
-                                                #/html/body/div[4]/main/div[1]/div/div[4]/div/div/div[2]/div[1]/div[1]/section/li/div/article/div[1]/div[2]/section/div[2]/article/div/div[2]/div/div/div/strong
-    #hotelLocations = driver.find_elements_by_xpath('/html/body/div[4]/main/div[1]/div/div[4]/div/div/div[2]/div[1]/div/section/ol/li/div/article/div[1]/div[2]/div/div/div//p')
-                                                   #/html/body/div[4]/main/div[1]/div/div[4]/div/div/div[2]/div[1]/div[1]/section/ol/li[4]/div/article/div[1]/div[2]/div/div/div[2]/div/p
+                                                
     hotelRates = driver.find_elements_by_xpath('//ol/li/div/article/div[1]/div[2]/div/div/button/span[1]/span[1]/span')
 
     print(url)
@@ -81,7 +75,6 @@ def get_hotels(place, date1, date2, noAdults, noChilds, resultSize, maxPrice, so
         print(hotelTypes[i].text)
         print(hotelPhotos[i].get_attribute('src'))
         print(hotelPrices[i].text)
-        #print(hotelLocations[i].text)
         print(hotelRates[i].text)
 
 get_hotels(sys.argv[5], sys.argv[2], sys.argv[3], int(sys.argv[6]), 0, 20 ,int(sys.argv[4]), sys.argv[1])
